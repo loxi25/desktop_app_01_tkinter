a76def2f77231e425df917a0af94ddcbc878d17a
loxi25 <oscarsanchez1@colegioguanenta.edu.co>
2025-07-31 15:56:02 -0500

    rtgfhcj

---
+++ ventana_1.py
@@ -0,0 +1,28 @@
+# se importala libreria tkinter con todas sus funciones 
+from tkinter import *
+
+# _________________________________
+# funciones de la app 
+#__________________________________
+
+
+
+#_____________________________
+#  ventana principal de la aplicacion
+#_____________________________
+
+
+# se declara una variable llamada ventana-principal , que adquiere las caracteristicas en un objeto de tipo TK()
+
+
+ventana_principal = Tk()
+
+# titulo de la ventana
+ventana_principal.title("oscar eduardo sanchez payares") 
+
+# tamaño de la ventana 
+ventana_principal.geometry("800x500")
+
+
+# metodo principal que despliega la ventana en pantalla
+ventana_principal.mainloop()
